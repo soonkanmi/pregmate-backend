c048debef3ec0db690f2f84ccdd3b0a7d4c939af
Olasunkanmi Oluwadara <geodebby4luv@yahoo.com>
2022-04-22 07:47:45 +0100

    updated dashboard screen

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -1,17 +1,14 @@
 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-    <div>
-        <x-jet-application-logo class="block h-12 w-auto" />
+    <div class="mt-8 mb-8 text-2xl">
+        Welcome {{ auth()->user()->name }}
     </div>
 
-    <div class="mt-8 text-2xl">
-        Welcome to {{ auth()->user()->name }}
-    </div>
-
-    <div class="mt-6 text-gray-500">
-        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-        ecosystem to be a breath of fresh air. We hope you love it.
+    <div class="mt-6 mb-8 text-gray-500">
+        @if (auth()->user()->role === 'admin')
+            You will be able to manage all application Users including Doctors and Administrators.
+        @else
+            As a Doctor, you will be able to manage Users' accounts
+        @endif
     </div>
 </div>
 {{--
